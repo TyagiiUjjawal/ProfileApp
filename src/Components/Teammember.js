be435306a4c48b7069976ad8698adf5fa08c7773
Ujjawal Tyagi <tyagiujjawal9@gmail.com>
2023-07-19 00:44:34 +0530

    Update Teammember.js

--- src/Components/Teammember.js
+++ src/Components/Teammember.js
@@ -1,134 +1,171 @@
-import React, { useEffect } from 'react'
-import styled from 'styled-components'
-import Popup from 'reactjs-popup';
-import FaIcon from 'react-fa-icon/lib/FaIcon';
-import {FaGithub,FaMailBulk,FaLinkedin} from 'react-icons/fa'
-const Teammember = ({menuData}) => {
+import React from "react";
+import styled from "styled-components";
+import Popup from "reactjs-popup";
+import { FaGithub, FaMailBulk, FaLinkedin } from "react-icons/fa";
+
+const Teammember = ({ menuData }) => {
   const Wrapper = styled.div`
+    .flex-container {
+      display: flex;
+      /* background-color: "black"; */
+      background: rgb(175, 30, 30);
+      /* flex-direction: column; */
+      flex-wrap: wrap;
+      justify-content: center;
+      /* gap: 19px; */
+      max-width: 700px;
+      margin: 0 auto;
+      margin-top: 60px;
+    }
 
-.flex-container {
-  display: flex;
-  column-gap: 19px;
-  max-width: 1200px;
-  width: 180%;
-  margin: 0 auto;
-  margin-top: 60px;
-}
-.space-between {
-  justify-content: space-between;
-}
-.col {
-  width: 60%;
-  
-}
-.card {
-  border: 1px solid #eee;
-  border-radius: 15px;
-  padding: 12px;
-  background-color: #fff;
-  display: flex;
-  column-gap: 15px;
-  box-shadow: 1px 1px 16px -6px rgba(0, 0, 0, 0.5);
-  -webkit-box-shadow: 1px 1px 16px -6px rgba(0, 0, 0, 0.5);
-  -moz-box-shadow: 1px 1px 16px -6px rgba(0, 0, 0, 0.5);
-}
-.card img {
-  
-  max-width: 250px;
-}
-.img-placeholder {
-  position: relative;
-  max-height: 250px;
-  overflow: hidden;
-  border-radius: 15px;
-  box-shadow: 1px 1px 16px -6px rgba(0, 0, 0, 0.75);
-  -webkit-box-shadow: 1px 1px 16px -6px rgba(0, 0, 0, 0.75);
-  -moz-box-shadow: 1px 1px 16px -6px rgba(0, 0, 0, 0.75);
-}
-h3 {
-  font-weight: 5500;
-}
-.flex{
+    .col {
+      width: 100%;
+      max-width: 380px;
+      border: 1px solid #eee;
+      border-radius: 15px;
 
-  display: flex;
-  flex-wrap: wrap;
-  
-}
-p {
-  font-weight: 400;
-}
-a {
-  color: #888;
-  text-decoration: none;
-}
-a:hover {
-  color: inherit;
-}
-#rol{
-  margin-top :15px;
-  margin-right: 10px;
-}
+      padding: 12px;
+      /* background-color: #fff; */
+      box-shadow: 1px 1px 16px -6px rgba(0, 0, 0, 0.5);
+    }
 
+    .card img {
+      /* width: 100%; */
+      max-width: 350px;
 
-.outerdiv{
-  height: 30px;
-}
+      /* position: cover; */
+      /* height: auto; */
+    }
+
+    .img-placeholder {
+      position: relative;
+      max-height: 250px;
+      overflow: hidden;
+      border-radius: 15px;
+      box-shadow: 1px 1px 16px -6px rgba(0, 0, 0, 0.75);
+    }
+
+    h3 {
+      font-weight: 500;
+      margin-top: 10px;
+    }
+
+    p {
+      font-weight: 400;
+    }
+
+    a {
+      color: #888;
+      text-decoration: none;
+    }
+
+    a:hover {
+      color: inherit;
+    }
+
+    .outerdiv {
+      margin-top: 15px;
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+    }
+
+    .icon {
+      margin-right: 10px;
+    }
+    @media (max-width: 768px) {
+      .flex-container {
+        display: flex;
+        /* margin-left: -330px; */
+
+        background-color: black;
+      }
+
+      .img {
+        background-color: blue;
+      }
+    }
   `;
-   
+
   return (
-   <>
-
-    <div class="grid">
-    <ul id="hexGrid">
-     {menuData.map((currElem)=>{
-    return(
-     
-      <li class="hex" key={currElem.id}>
-        <div  class="hexIn">
-      
-          <a class="hexLink" href="#">
-          <Popup trigger={            <div class='img' style={{backgroundImage: `url("${currElem.image}")` }}></div>
-} 
-     position="right center">
-     <Wrapper>
-     <div class="flex-container space-between">
-  <div class="col card">
-    <div class="img-placeholder">
-      <img src={currElem.image}/>
-    </div>
-    <div className="outerdiv" >
-      <h3>{currElem.name}</h3>
-    
-        <p> <b> Role:</b> Web loper</p>
-        <p> <b> Graduation:</b> 2023</p>
-        <p> <b> Branch:</b> IT</p>
-        <p> <b> Department:</b> DAQ•WebD</p>
-      <a target={"_blank"} href=""><FaGithub/></a>  
-      <a style={{marginLeft:'20px'}} target={"_blank"} href=""><FaMailBulk/></a>  
-      <a style={{marginLeft:'20px'}} target={"_blank"} href=""><FaLinkedin/></a>  
-    </div>
-  </div>
- 
-</div>
-</Wrapper>
-    </Popup>
-            <h1 id="demo1">{currElem.name}</h1>
-            <p id="demo2">{currElem.branch}</p>
-          </a>
-        </div>
-      </li>
-            ) })}
-     
-    </ul>
+    <>
+      <div className="grid">
+        <ul id="hexGrid">
+          {menuData.map((currElem) => {
+            return (
+              <li className="hex" key={currElem.id}>
+                <div className="hexIn">
+                  <a className="hexLink" href="#">
+                    <Popup
+                      trigger={
+                        <div
+                          className="img"
+                          style={{
+                            backgroundImage: `url("${currElem.image}")`,
+                          }}
+                        ></div>
+                      }
+                      position=" center"
+                    >
+                      <Wrapper>
+                        <div className="flex-container">
+                          <div className="col card">
+                            <div className="img-placeholder">
+                              <img src={currElem.image} alt="Team Member" />
+                            </div>
+                            <div className="outerdiv">
+                              <h3>{currElem.name}</h3>
+                              <p>
+                                <b>Role:</b> Web Developer
+                              </p>
+                              <p>
+                                <b>Graduation:</b> 2023
+                              </p>
+                              <p>
+                                <b>Branch:</b> IT
+                              </p>
+                              <p>
+                                <b>Department:</b> DAQ•WebD
+                              </p>
+                              <div>
+                                <a
+                                  target="_blank"
+                                  rel="noopener noreferrer"
+                                  href="#"
+                                >
+                                  <FaGithub className="icon" />
+                                </a>
+                                <a
+                                  target="_blank"
+                                  rel="noopener noreferrer"
+                                  href="#"
+                                >
+                                  <FaMailBulk className="icon" />
+                                </a>
+                                <a
+                                  target="_blank"
+                                  rel="noopener noreferrer"
+                                  href="#"
+                                >
+                                  <FaLinkedin className="icon" />
+                                </a>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </Wrapper>
+                    </Popup>
+                    <h1 id="demo1">{currElem.name}</h1>
+                    <p id="demo2">{currElem.branch}</p>
+                  </a>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
       </div>
-	
-	
-	
+    </>
+  );
+};
 
-
-   
-   </>
-  )
-}
-
-export default Teammember
+export default Teammember;
